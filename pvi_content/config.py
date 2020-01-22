f04b1a6317a8500e79fd72f1d30ffa5065a3b901
youqiongsz <youqiongsz@gmail.com>
2020-01-22 14:59:19 +0800

    add content

---
+++ pvi_content/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding=utf8
+
+'''
+  Copyright (C) 2020, by Zhan Shi (youqiong)
+  Filename : config2.py
+  Date     : 2020-01-22 14:40:52
+  Email    : youqinogsz@gmail.com
+'''
+
+default_config = {
+        "name" : "Zhan Shi",
+        "nickname" : "youqiong",
+        "email" : "youqinogsz@gmail.com",
+}
+
+default_template = {
+        "cpp" : "cplusplus_default",
+        "py" : "python_default",
+        "sh" : "shell_default",
+        "html" : "html_default",
+}
+
+special_type = {
+        "pwn" : "python_pwn",
+        "cf" : "cplusplus_codeforces",
+}
